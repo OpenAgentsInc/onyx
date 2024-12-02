be72b33474b4bfd920bd3d87319c9846e9df2b03
Christopher David <chris@openagents.com>
2024-12-02 14:58:27 -0600

    Refactored MainNavigator.tsx to import AppStackScreenProps from './AppNavigator' for better type consistency.

--- app/navigators/MainNavigator.tsx
+++ app/navigators/MainNavigator.tsx
@@ -6,7 +6,6 @@ import {
   createBottomTabNavigator,
 } from "@react-navigation/bottom-tabs"
 import { CompositeScreenProps } from "@react-navigation/native"
-import { Icon } from "../components"
 import {
   HomeScreen,
   CommunityScreen,
@@ -15,6 +14,7 @@ import {
   ProfileScreen,
 } from "../screens"
 import { AppStackParamList, AppStackScreenProps } from "./AppNavigator"
+import { CustomTabBar } from "@/components/CustomTabBar"
 
 import type { ThemedStyle } from "@/theme"
 
@@ -42,75 +42,36 @@ export function MainNavigator() {
 
   return (
     <Tab.Navigator
+      tabBar={(props) => <CustomTabBar {...props} />}
       screenOptions={{
         headerShown: false,
         tabBarHideOnKeyboard: true,
-        tabBarStyle: themed([$tabBar, { height: bottom + 50 }]),
-        tabBarActiveTintColor: colors.text,
-        tabBarInactiveTintColor: colors.text,
-        tabBarShowLabel: false,
-        tabBarItemStyle: themed($tabBarItem),
       }}
     >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Icon icon="home" color={focused ? colors.tint : colors.tintInactive} size={28} />
-          ),
-        }}
       />
 
       <Tab.Screen
         name="Community"
         component={CommunityScreen}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Icon icon="groups" color={focused ? colors.tint : colors.tintInactive} size={28} />
-          ),
-        }}
       />
 
       <Tab.Screen
         name="Onyx"
         component={OnyxScreen}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Icon icon="mic" color={focused ? colors.tint : colors.tintInactive} size={28} />
-          ),
-        }}
       />
 
       <Tab.Screen
         name="Wallet"
         component={WalletScreen}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Icon icon="account-balance-wallet" color={focused ? colors.tint : colors.tintInactive} size={28} />
-          ),
-        }}
       />
 
       <Tab.Screen
         name="Profile"
         component={ProfileScreen}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Icon icon="person" color={focused ? colors.tint : colors.tintInactive} size={28} />
-          ),
-        }}
       />
     </Tab.Navigator>
   )
 }
-
-const $tabBar: ThemedStyle<ViewStyle> = ({ colors }) => ({
-  backgroundColor: colors.background,
-  borderTopColor: colors.separator,
-  paddingVertical: 8,
-})
-
-const $tabBarItem: ThemedStyle<ViewStyle> = () => ({
-  padding: 0,
-})
