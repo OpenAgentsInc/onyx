d7d89da4c7b7ad043d896ee1a3ff1b827865a6a1
Christopher David <chris@openagents.com>
2024-12-03 09:43:53 -0600

    Add HudButtons component and mic press handler to OnyxScreen.

--- app/screens/OnyxScreen.tsx
+++ app/screens/OnyxScreen.tsx
@@ -3,13 +3,28 @@ import { FC } from "react"
 import { View, StyleSheet } from "react-native"
 import { MainTabScreenProps } from "@/navigators"
 import { Canvas } from "@/components/Canvas"
+import { HudButtons } from "@/components/HudButtons"
 
 interface OnyxScreenProps extends MainTabScreenProps<"Onyx"> { }
 
 export const OnyxScreen: FC<OnyxScreenProps> = observer(function OnyxScreen() {
+  const handleMicPress = () => {
+    // TODO: Implement mic functionality
+    console.log("Mic pressed")
+  }
+
+  const handleChatPress = () => {
+    // TODO: Implement chat functionality
+    console.log("Chat pressed")
+  }
+
   return (
     <View style={styles.container}>
       <Canvas />
+      <HudButtons
+        onMicPress={handleMicPress}
+        onChatPress={handleChatPress}
+      />
     </View>
   )
 })
