395c63bb5a4176fdcea893e7b86f33cbe6cdd987
Christopher David <chris@openagents.com>
2024-12-03 16:29:15 -0600

    Added ChatStoreModel to RootStore for chat feature integration.

--- app/models/RootStore.ts
+++ app/models/RootStore.ts
@@ -2,6 +2,7 @@ import { Instance, SnapshotOut, types } from "mobx-state-tree"
 import { AuthenticationStoreModel } from "./AuthenticationStore"
 import { EpisodeStoreModel } from "./EpisodeStore"
 import { RecordingStoreModel } from "./RecordingStore"
+import { ChatStoreModel } from "./ChatStore"
 
 /**
  * A RootStore model.
@@ -10,6 +11,7 @@ export const RootStoreModel = types.model("RootStore").props({
   authenticationStore: types.optional(AuthenticationStoreModel, {}),
   episodeStore: types.optional(EpisodeStoreModel, {}),
   recordingStore: types.optional(RecordingStoreModel, {}),
+  chatStore: types.optional(ChatStoreModel, { messages: [] }),
 })
 
 /**
