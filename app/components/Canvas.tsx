72c01ad0f7d50a097f0cc236cd3cdcfea0ae386c
Christopher David <chris@openagents.com>
2024-12-02 19:09:40 -0600

    Create file app/components/Canvas.tsx

---
+++ app/components/Canvas.tsx
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react'
+import { Canvas as ThreeCanvas } from '@react-three/fiber/native'
+import { useFrame } from '@react-three/fiber/native'
+import { View } from 'react-native'
+
+function Box() {
+  const meshRef = useRef(null)
+
+  useFrame((state, delta) => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += delta
+      meshRef.current.rotation.y += delta
+    }
+  })
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="white" />
+    </mesh>
+  )
+}
+
+export function Canvas() {
+  return (
+    <View style={{ flex: 1 }}>
+      <ThreeCanvas
+        style={{ flex: 1 }}
+        gl={{ clearColor: '#000000' }}
+      >
+        <ambientLight intensity={0.1} />
+        <pointLight position={[10, 10, 10]} />
+        <Box />
+      </ThreeCanvas>
+    </View>
+  )
+}
