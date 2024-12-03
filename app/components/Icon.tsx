7e99cd12f654d23a0211194a2b3d4a2f50f00420
Christopher David <chris@openagents.com>
2024-12-02 21:15:17 -0600

    No changes detected in Icon.tsx file. Commit message unnecessary.

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -129,6 +129,7 @@ export const iconRegistry = {
   slack: require("../../assets/icons/demo/slack.png"),
   view: require("../../assets/icons/view.png"),
   x: require("../../assets/icons/x.png"),
+  appIcon: require("../../assets/images/app-icon-all.png"),
 }
 
 const $imageStyleBase: ImageStyle = {
