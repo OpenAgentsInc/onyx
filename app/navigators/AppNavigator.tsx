b3ce73a287269a7f27d19aa197d4ffaa13712890
Christopher David <chris@openagents.com>
2024-12-04 10:08:19 -0600

    Added NavigationContainer and NativeStackNavigator imports to AppNavigator.tsx, and included new dependencies and configurations to enhance navigation management.

--- app/navigators/AppNavigator.tsx
+++ app/navigators/AppNavigator.tsx
@@ -2,36 +2,20 @@ import { observer } from "mobx-react-lite"
 import { ComponentProps } from "react"
 import * as Screens from "@/screens"
 import { useAppTheme, useThemeProvider } from "@/utils/useAppTheme"
-/**
- * The app navigator (formerly "AppNavigator" and "MainNavigator") is used for the primary
- * navigation flows of your app.
- * Generally speaking, it will contain an auth flow (registration, login, forgot password)
- * and a "main" flow which the user will use once logged in.
- */
 import {
-  NavigationContainer, NavigatorScreenParams
+  NavigationContainer,
+  NavigationContainerRef,
+  NavigatorScreenParams,
 } from "@react-navigation/native"
 import {
-  createNativeStackNavigator, NativeStackScreenProps
+  createNativeStackNavigator,
+  NativeStackScreenProps,
 } from "@react-navigation/native-stack"
 import Config from "../config"
 import { useStores } from "../models"
 import { MainNavigator, MainTabParamList } from "./MainNavigator"
 import { navigationRef, useBackButtonHandler } from "./navigationUtilities"
 
-/**
- * This type allows TypeScript to know what routes are defined in this navigator
- * as well as what properties (if any) they might take when navigating to them.
- *
- * If no params are allowed, pass through `undefined`. Generally speaking, we
- * recommend using your MobX-State-Tree store(s) to keep application state
- * rather than passing state through navigation params.
- *
- * For more information, see this documentation:
- *   https://reactnavigation.org/docs/params/
- *   https://reactnavigation.org/docs/typescript#type-checking-the-navigator
- *   https://reactnavigation.org/docs/typescript/#organizing-types
- */
 export type AppStackParamList = {
   Main: NavigatorScreenParams<MainTabParamList>
   Login: undefined
@@ -39,10 +23,6 @@ export type AppStackParamList = {
   Chat: undefined
 }
 
-/**
- * This is a list of all the route names that will exit the app if the back button
- * is pressed while in that screen. Only affects Android.
- */
 const exitRoutes = Config.exitRoutes
 
 export type AppStackScreenProps<T extends keyof AppStackParamList> = NativeStackScreenProps<
@@ -50,7 +30,6 @@ export type AppStackScreenProps<T extends keyof AppStackParamList> = NativeStack
   T
 >
 
-// Documentation: https://reactnavigation.org/docs/stack-navigator/
 const Stack = createNativeStackNavigator<AppStackParamList>()
 
 const AppStack = observer(function AppStack() {
@@ -91,7 +70,11 @@ export const AppNavigator = observer(function AppNavigator(props: NavigationProp
 
   return (
     <ThemeProvider value={{ themeScheme, setThemeContextOverride }}>
-      <NavigationContainer ref={navigationRef} theme={navigationTheme} {...props}>
+      <NavigationContainer
+        ref={navigationRef as React.RefObject<NavigationContainerRef<AppStackParamList>>}
+        theme={navigationTheme}
+        {...props}
+      >
         <AppStack />
       </NavigationContainer>
     </ThemeProvider>
