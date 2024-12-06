11f42cf525e08a099da418190ef9b37e8c0efe98
Christopher David <chris@openagents.com>
2024-12-06 16:35:48 -0600

    Refactor index.ts: Remove unused exports, add PylonDemoScreen, and modify ChatScreen export for clarity and optimization.

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -1,12 +1,5 @@
 export * from "./WelcomeScreen"
 export * from "./LoginScreen"
-export * from "./ErrorScreen/ErrorBoundary"
-export * from "./ChatScreen"
-
-// Main tab screens
-export * from "./HomeScreen"
-export * from "./CommunityScreen"
 export * from "./OnyxScreen"
-export * from "./WalletScreen"
-export * from "./ProfileScreen"
-export * from "./UpdaterScreen"
+export * from "./PylonDemoScreen"
+export { default as ChatScreen } from "./ChatScreen"
