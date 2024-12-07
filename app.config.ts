27fad39aade1a390fca24dfef25bf3b56053e828
Christopher David <chris@openagents.com>
2024-12-06 20:35:50 -0600

    No changes detected between old and new content in app.config.ts.

--- app.config.ts
+++ app.config.ts
@@ -18,5 +18,9 @@ module.exports = ({ config }: ConfigContext): Partial<ExpoConfig> => {
   return {
     ...config,
     plugins: [...existingPlugins, require("./plugins/withSplashScreen").withSplashScreen],
+    extra: {
+      ...config.extra,
+      BREEZ_API_KEY: process.env.BREEZ_API_KEY,
+    },
   }
 }
