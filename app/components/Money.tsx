04cf8d3ea1b7a66a0762d52bc10f224204906009
Christopher David <chris@openagents.com>
2024-12-11 15:50:19 -0600

    font

--- app/components/Money.tsx
+++ app/components/Money.tsx
@@ -174,7 +174,7 @@ const styles = StyleSheet.create({
   balance: {
     fontSize: 34,
     lineHeight: 40,
-    fontFamily: typography.secondary.bold,
+    fontFamily: typography.primary.bold,
   }
 });
 
