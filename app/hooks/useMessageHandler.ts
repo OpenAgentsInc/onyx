10c72c5c5b919c432f51c2b447bf99161fcfbeca
Christopher David <chris@openagents.com>
2024-12-14 23:23:44 -0600

    Added logging in `useMessageHandler` for setting and calling message handlers.

--- app/hooks/useMessageHandler.ts
+++ app/hooks/useMessageHandler.ts
@@ -5,16 +5,27 @@ let handleMessageCallback: HandleMessageFunction | null = null
 
 export const useMessageHandler = () => {
   const setHandleMessage = (callback: HandleMessageFunction) => {
+    console.log("Setting message handler", !!callback)
     handleMessageCallback = callback
   }
 
   const handleMessage = async (text: string) => {
+    console.log("handleMessage called with:", text)
     if (!handleMessageCallback) {
       console.error('No message handler set')
       return
     }
 
-    await handleMessageCallback({ text })
+    try {
+      const partialMessage: MessageType.PartialText = {
+        text,
+        type: 'text'
+      }
+      console.log("Calling message handler with:", partialMessage)
+      await handleMessageCallback(partialMessage)
+    } catch (err) {
+      console.error("Error in handleMessage:", err)
+    }
   }
 
   return {
