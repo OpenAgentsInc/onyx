a29917617400f2c495211935ee6b56b41af97c1a
GitHub API <noreply@github.com>
2024-12-15 10:15:35 -0600

    Delete app/screens/LlamaRNExample.tsx

--- app/screens/LlamaRNExample.tsx
+++
@@ -1,9 +0,0 @@
-import { observer } from "mobx-react-lite"
-import React from "react"
-import { ChatContainer } from "@/components/chat/ChatContainer"
-
-export const LlamaRNExample = observer(function LlamaRNExample() {
-  return <ChatContainer />
-})
-
-export default LlamaRNExample
