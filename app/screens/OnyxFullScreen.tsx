974072adf15dca807b842dc11bfb95b701081813
Christopher David <chris@openagents.com>
2024-12-15 19:06:12 -0600

    shh

--- app/screens/OnyxFullScreen.tsx
+++ app/screens/OnyxFullScreen.tsx
@@ -1,18 +1,13 @@
 import { observer } from "mobx-react-lite"
 import { FC } from "react"
 import { View, ViewStyle } from "react-native"
-import { Screen, Text } from "@/components"
 import { Canvas } from "@/components/Canvas"
 import { useAutoUpdate } from "@/hooks/useAutoUpdate"
 import { AppStackScreenProps } from "@/navigators"
 
-// import { useNavigation } from "@react-navigation/native"
-// import { useStores } from "@/models"
-
 interface OnyxFullScreenProps extends AppStackScreenProps<"OnyxFull"> { }
 
 export const OnyxFullScreen: FC<OnyxFullScreenProps> = observer(function OnyxFullScreen() {
-  // Initialize auto-update functionality
   useAutoUpdate();
 
   return (
