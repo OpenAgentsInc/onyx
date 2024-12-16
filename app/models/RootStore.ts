ade23117f802a6e9cfaac1ec14c7cae089376674
Christopher David <chris@openagents.com>
2024-12-15 20:49:20 -0600

    Refactor RootStore.ts: Removed AuthenticationStore, UserStore, WalletStore; added LlamaContext to ModelStore.

--- app/models/RootStore.ts
+++ app/models/RootStore.ts
@@ -1,34 +1,33 @@
 import { Instance, SnapshotOut, types } from "mobx-state-tree"
-import { AuthenticationStoreModel } from "./AuthenticationStore"
 import { ChatStoreModel } from "./ChatStore"
-import { ModelStoreModel } from "./ModelStore"
 import { RecordingStoreModel } from "./RecordingStore"
-import { UserStoreModel } from "./UserStore"
-import { WalletStoreModel } from "./WalletStore"
+import type { LlamaContext } from "llama.rn"
+
+export const ModelStoreModel = types
+  .model("ModelStore")
+  .props({
+    context: types.frozen<LlamaContext | null>(null),
+  })
+  .actions((store) => ({
+    setContext(context: LlamaContext | null) {
+      store.context = context
+    },
+  }))
 
 /**
  * A RootStore model.
  */
 export const RootStoreModel = types.model("RootStore").props({
-  authenticationStore: types.optional(AuthenticationStoreModel, {}),
+  chatStore: types.optional(ChatStoreModel, {}),
   recordingStore: types.optional(RecordingStoreModel, {}),
-  chatStore: types.optional(ChatStoreModel, { messages: [], showFullChat: false }),
-  walletStore: types.optional(WalletStoreModel, {
-    balanceSat: 0,
-    pendingSendSat: 0,
-    pendingReceiveSat: 0,
-    transactions: [],
-  }),
-  userStore: types.optional(UserStoreModel, {}),
   modelStore: types.optional(ModelStoreModel, {}),
 })
 
 /**
  * The RootStore instance.
  */
-export interface RootStore extends Instance<typeof RootStoreModel> { }
-
+export interface RootStore extends Instance<typeof RootStoreModel> {}
 /**
  * The data of a RootStore.
  */
-export interface RootStoreSnapshot extends SnapshotOut<typeof RootStoreModel> { }
+export interface RootStoreSnapshot extends SnapshotOut<typeof RootStoreModel> {}
