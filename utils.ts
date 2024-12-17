d7fc80022b27875c8607aa37dc481b1d08085fa0
Christopher David <chris@openagents.com>
2024-12-17 12:38:31 -0600

    Refactor `generateAPIUrl` to cache origin and use `manifest2` for debuggerHost in development.

--- utils.ts
+++ utils.ts
@@ -1,14 +1,26 @@
 import Constants from "expo-constants"
 
+let cachedOrigin: string | null = null;
+
 export const generateAPIUrl = (relativePath: string) => {
   const path = relativePath.startsWith('/') ? relativePath : `/${relativePath}`;
 
   if (process.env.NODE_ENV === 'development') {
-    // Default to localhost if experienceUrl is not available
-    const origin = Constants.experienceUrl 
-      ? Constants.experienceUrl.replace('exp://', 'http://')
-      : 'http://localhost:3000';
-    return origin.concat(path);
+    // Use cached origin if we have it
+    if (!cachedOrigin) {
+      // On first run, prefer the manifest URL if available
+      if (Constants.manifest2?.extra?.expoClient?.debuggerHost) {
+        const debuggerHost = Constants.manifest2.extra.expoClient.debuggerHost;
+        cachedOrigin = `http://${debuggerHost.split(':')[0]}:3000`;
+      } else {
+        cachedOrigin = 'http://localhost:3000';
+      }
+      console.log('Setting cached API origin to:', cachedOrigin);
+    }
+    
+    const finalUrl = cachedOrigin.concat(path);
+    console.log('Using API URL:', finalUrl);
+    return finalUrl;
   }
 
   if (!process.env.EXPO_PUBLIC_API_BASE_URL) {
