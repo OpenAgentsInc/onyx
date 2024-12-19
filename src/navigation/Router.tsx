68eb7e7a24d87a0581294c658e941647c8972565
Christopher David <chris@openagents.com>
2024-12-19 06:45:23 -0600

    hide canvas again on emulator

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -83,6 +83,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'transparent',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   content: {
     padding: 20,
