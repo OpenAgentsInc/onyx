8bdf353805afd6b2624b921b2b27cf182039d86a
Christopher David <chris@openagents.com>
2024-12-22 00:55:35 -0600

    simple root store

--- src/models/RootStore.ts
+++ src/models/RootStore.ts
@@ -1,26 +1,21 @@
 import { Instance, SnapshotOut, types } from "mobx-state-tree"
-import { AuthenticationStoreModel } from "./AuthenticationStore"
-import { ChatStoreModel } from "./ChatStore"
-import { ModelStoreModel } from "./ModelStore"
-import { RecordingStoreModel } from "./RecordingStore"
-import { UserStoreModel } from "./UserStore"
-import { WalletStoreModel } from "./WalletStore"
 
 /**
  * A RootStore model.
  */
 export const RootStoreModel = types.model("RootStore").props({
-  authenticationStore: types.optional(AuthenticationStoreModel, {}),
-  recordingStore: types.optional(RecordingStoreModel, {}),
-  chatStore: types.optional(ChatStoreModel, { messages: [], showFullChat: false }),
-  walletStore: types.optional(WalletStoreModel, {
-    balanceSat: 0,
-    pendingSendSat: 0,
-    pendingReceiveSat: 0,
-    transactions: [],
-  }),
-  userStore: types.optional(UserStoreModel, {}),
-  modelStore: types.optional(ModelStoreModel, {}),
+  test: types.optional(types.string, "test"),
+  // authenticationStore: types.optional(AuthenticationStoreModel, {}),
+  // recordingStore: types.optional(RecordingStoreModel, {}),
+  // chatStore: types.optional(ChatStoreModel, { messages: [], showFullChat: false }),
+  // walletStore: types.optional(WalletStoreModel, {
+  //   balanceSat: 0,
+  //   pendingSendSat: 0,
+  //   pendingReceiveSat: 0,
+  //   transactions: [],
+  // }),
+  // userStore: types.optional(UserStoreModel, {}),
+  // modelStore: types.optional(ModelStoreModel, {}),
 })
 
 /**
