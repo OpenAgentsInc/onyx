0a974fd5f0a278f41192c8d3df7cd0e2aeb365a0
Christopher David <chris@openagents.com>
2024-12-21 20:13:24 -0600

    two icons

--- src/onyx/OnyxLayout.tsx
+++ src/onyx/OnyxLayout.tsx
@@ -1,12 +1,27 @@
 import { Image, TouchableOpacity } from "react-native"
 
 export const OnyxLayout = () => {
+  const ICON_SIZE = 56
   return (
-    <TouchableOpacity
-      activeOpacity={0.8}
-      style={{ position: "absolute", bottom: 50, right: 30, zIndex: 8 }}
-    >
-      <Image source={require("../../assets/images/voice.png")} style={{ width: 50, height: 50 }} />
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={{ position: "absolute", bottom: 50, left: 40, zIndex: 8 }}
+      >
+        <Image
+          source={require("../../assets/icons/text.png")}
+          style={{ width: ICON_SIZE, height: ICON_SIZE }}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={{ position: "absolute", bottom: 50, right: 40, zIndex: 8 }}
+      >
+        <Image
+          source={require("../../assets/icons/voice.png")}
+          style={{ width: ICON_SIZE, height: ICON_SIZE }}
+        />
+      </TouchableOpacity>
+    </>
   )
 }
