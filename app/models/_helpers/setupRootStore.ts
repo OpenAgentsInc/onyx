1b1a6aad0409ef4c4b9b644483122767d91f35dd
Christopher David <chris@openagents.com>
2024-12-22 21:50:06 -0600

    Refactor setupRootStore.ts to streamline RootStore rehydration and update imports.

--- app/models/_helpers/setupRootStore.ts
+++ app/models/_helpers/setupRootStore.ts
@@ -1,51 +1,25 @@
-/**
- * This file is where we do "rehydration" of your RootStore from AsyncStorage.
- * This lets you persist your state between app launches.
- *
- * Navigation state persistence is handled in navigationUtilities.tsx.
- *
- * Note that Fast Refresh doesn't play well with this file, so if you edit this,
- * do a full refresh of your app instead.
- *
- * @refresh reset
- */
-import { applySnapshot, IDisposer, onSnapshot } from "mobx-state-tree"
+import { onSnapshot } from "mobx-state-tree"
+import { RootStore, RootStoreModel, RootStoreSnapshotIn } from "../RootStore"
 import * as storage from "../../utils/storage"
-import { RootStore, RootStoreSnapshot } from "../RootStore"
 
-/**
- * The key we'll be saving our state as within async storage.
- */
-const ROOT_STATE_STORAGE_KEY = "root-v1aa1abe31eee1211f"
+const ROOT_STATE_STORAGE_KEY = "root"
 
-/**
- * Setup the root state.
- */
-let _disposer: IDisposer | undefined
-export async function setupRootStore(rootStore: RootStore) {
-  let restoredState: RootStoreSnapshot | undefined | null
+export async function setupRootStore() {
+  let rootStore: RootStore
+  let data: RootStoreSnapshotIn
 
   try {
-    // load the last known state from AsyncStorage
-    restoredState = ((await storage.load(ROOT_STATE_STORAGE_KEY)) ?? {}) as RootStoreSnapshot
-    applySnapshot(rootStore, restoredState)
+    // Load the last known state from storage
+    data = (await storage.load(ROOT_STATE_STORAGE_KEY)) || {}
+    rootStore = RootStoreModel.create(data)
   } catch (e) {
-    // if there's any problems loading, then inform the dev what happened
-    if (__DEV__) {
-      if (e instanceof Error) console.error(e.message)
-    }
+    // If there's any problems loading, then inform the dev what happened
+    console.error(e.message, null)
+    rootStore = RootStoreModel.create({})
   }
 
-  // stop tracking state changes if we've already setup
-  if (_disposer) _disposer()
+  // Track changes & save to storage
+  onSnapshot(rootStore, (snapshot) => storage.save(ROOT_STATE_STORAGE_KEY, snapshot))
 
-  // track changes & save to AsyncStorage
-  _disposer = onSnapshot(rootStore, (snapshot) => storage.save(ROOT_STATE_STORAGE_KEY, snapshot))
-
-  const unsubscribe = () => {
-    _disposer?.()
-    _disposer = undefined
-  }
-
-  return { rootStore, restoredState, unsubscribe }
+  return rootStore
 }
