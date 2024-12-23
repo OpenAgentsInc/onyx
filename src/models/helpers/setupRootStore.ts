4c3fa9e7446ea2e0368f6ff6414af77d915e7186
Christopher David <chris@openagents.com>
2024-12-22 19:26:27 -0600

    axe old model store

--- src/models/helpers/setupRootStore.ts
+++ src/models/helpers/setupRootStore.ts
@@ -16,7 +16,7 @@ import { RootStore, RootStoreSnapshot } from "../RootStore"
 /**
  * The key we'll be saving our state as within async storage.
  */
-const ROOT_STATE_STORAGE_KEY = "root-v1aa1ab31111"
+const ROOT_STATE_STORAGE_KEY = "root-v1aa1ab311211"
 
 /**
  * Setup the root state.
