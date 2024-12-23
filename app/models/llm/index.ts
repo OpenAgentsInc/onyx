1afa6057d613d57bc6407f46f9cb36e936d24f01
GitHub API <noreply@github.com>
2024-12-22 21:04:36 -0600

    Delete app/models/llm/index.ts

--- app/models/llm/index.ts
+++
@@ -1,3 +0,0 @@
-export { LLMStoreModel, createLLMStoreDefaultModel } from "./store"
-export type { LLMStore, LLMStoreSnapshotIn, LLMStoreSnapshotOut } from "./store"
-export type { IModelInfo } from "./types"
