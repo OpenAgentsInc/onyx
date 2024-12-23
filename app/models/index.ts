4fb7788c06d354fe37c4daba84d393cc84b3b7f9
Christopher David <chris@openagents.com>
2024-12-22 21:53:25 -0600

    Refactor export statements in index.ts for clarity and restrict exported helpers to useStores and RootStoreProvider.

--- app/models/index.ts
+++ app/models/index.ts
@@ -1,4 +1,2 @@
 export * from "./RootStore"
-export * from "./_helpers/getRootStore"
-export * from "./_helpers/useStores"
-export * from "./_helpers/setupRootStore"
+export { useStores, RootStoreProvider } from "./_helpers/useStores"
