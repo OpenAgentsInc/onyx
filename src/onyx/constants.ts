be4c9c7e6f1f0361f1984696acfbf5bbeed78244
Christopher David <chris@openagents.com>
2024-12-22 18:43:03 -0600

    Create file src/onyx/constants.ts

---
+++ src/onyx/constants.ts
@@ -0,0 +1,19 @@
+export const CHAT_CONSTANTS = {
+  DEFAULT_SYSTEM_MESSAGE: "This is a conversation between user and assistant, a friendly chatbot.",
+  DEFAULT_TEMPERATURE: 0.7,
+  DEFAULT_TOP_K: 40,
+  DEFAULT_TOP_P: 0.5,
+  DEFAULT_MIN_P: 0.05,
+  MAX_TOKENS: 100,
+  STOP_SEQUENCES: [
+    "</s>",
+    "<|end|>",
+    "<|eot_id|>",
+    "<|end_of_text|>",
+    "<|im_end|>",
+    "<|EOT|>",
+    "<|END_OF_TURN_TOKEN|>",
+    "<|end_of_turn|>",
+    "<|endoftext|>",
+  ],
+}
