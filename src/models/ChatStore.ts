edf8bb1b76b06437c68141d4dc553c1ac7b397ad
Christopher David <chris@openagents.com>
2024-12-22 18:10:33 -0600

    chatstore model initial

---
+++ src/models/ChatStore.ts
@@ -0,0 +1,29 @@
+import { Instance, SnapshotIn, SnapshotOut, types } from "mobx-state-tree"
+import { log } from "@/utils/log"
+import { withSetPropAction } from "./helpers/withSetPropAction"
+
+export const ChatStoreModel = types
+  .model("ChatStore")
+  .props({
+    isInitialized: types.optional(types.boolean, false),
+    error: types.maybeNull(types.string),
+  })
+  .actions(withSetPropAction)
+  .actions((self) => {
+    const setup = () => {
+      log({ name: "[ChatStore] Setting up" })
+    }
+    return {
+      setup
+    }
+  })
+
+export interface ChatStore extends Instance<typeof ChatStoreModel> { }
+export interface ChatStoreSnapshotOut extends SnapshotOut<typeof ChatStoreModel> { }
+export interface ChatStoreSnapshotIn extends SnapshotIn<typeof ChatStoreModel> { }
+
+export const createWalletStoreDefaultModel = () =>
+  ChatStoreModel.create({
+    isInitialized: false,
+    error: null,
+  })
--- src/models/RootStore.ts
+++ src/models/RootStore.ts
@@ -1,10 +1,12 @@
 import { Instance, SnapshotOut, types } from "mobx-state-tree"
+import { ChatStoreModel } from "./ChatStore"
 import { LLMStoreModel } from "./LLMStore"
 
 /**
  * A RootStore model.
  */
 export const RootStoreModel = types.model("RootStore").props({
+  chatStore: types.optional(ChatStoreModel, {}),
   llmStore: types.optional(LLMStoreModel, {}),
 })
 
