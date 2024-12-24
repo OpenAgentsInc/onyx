885f4fd2cd202272182ae073cdaf2bd5ae5fa404
Christopher David <chris@openagents.com>
2024-12-24 09:50:39 -0600

    rootstoer

--- app/models/RootStore.ts
+++ app/models/RootStore.ts
@@ -1,18 +1,21 @@
 import { Instance, SnapshotIn, SnapshotOut, types } from "mobx-state-tree"
 import { ChatStoreWithActions } from "./chat/ChatStore"
 import { LLMStoreModel } from "./llm/LLMStore"
+import { ToolStoreModel } from "./tools"
 
 export const RootStoreModel = types.model("RootStore").props({
   chatStore: types.optional(ChatStoreWithActions, {}),
-  llmStore: types.optional(LLMStoreModel, {})
+  llmStore: types.optional(LLMStoreModel, {}),
+  toolStore: types.optional(ToolStoreModel, {})
 })
 
-export interface RootStore extends Instance<typeof RootStoreModel> {}
-export interface RootStoreSnapshotOut extends SnapshotOut<typeof RootStoreModel> {}
-export interface RootStoreSnapshotIn extends SnapshotIn<typeof RootStoreModel> {}
+export interface RootStore extends Instance<typeof RootStoreModel> { }
+export interface RootStoreSnapshotOut extends SnapshotOut<typeof RootStoreModel> { }
+export interface RootStoreSnapshotIn extends SnapshotIn<typeof RootStoreModel> { }
 
 export const createRootStoreDefaultModel = () =>
   RootStoreModel.create({
     chatStore: {},
-    llmStore: {}
+    llmStore: {},
+    toolStore: {}
   })
