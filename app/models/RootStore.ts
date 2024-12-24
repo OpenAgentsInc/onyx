f3c66139187af064c85213f2048618a2208040fb
Christopher David <chris@openagents.com>
2024-12-24 11:43:58 -0600

    Refactor RootStore.ts: Rename RootStore interface to IRootStore, change RootStoreModel from const to export.

--- app/models/RootStore.ts
+++ app/models/RootStore.ts
@@ -2,7 +2,7 @@ import { Instance, SnapshotIn, SnapshotOut, types } from "mobx-state-tree"
 import { ChatStoreWithActions } from "./chat/ChatStore"
 import { ToolStoreModel } from "./tools"
 
-export const RootStoreModel = types
+const RootStoreModel = types
   .model("RootStore")
   .props({
     chatStore: types.optional(ChatStoreWithActions, {}),
@@ -13,9 +13,12 @@ export const RootStoreModel = types
     })
   })
 
-export interface RootStore extends Instance<typeof RootStoreModel> { }
+export interface IRootStore extends Instance<typeof RootStoreModel> { }
 export interface RootStoreSnapshotOut extends SnapshotOut<typeof RootStoreModel> { }
 export interface RootStoreSnapshotIn extends SnapshotIn<typeof RootStoreModel> { }
+
+export { RootStoreModel }
+export type RootStore = Instance<typeof RootStoreModel>
 
 export const createRootStoreDefaultModel = () =>
   RootStoreModel.create({
