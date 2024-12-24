4a45c224b19408958481506d90e41e6b5a2be1bc
Christopher David <chris@openagents.com>
2024-12-24 10:06:51 -0600

    envs

--- app.config.ts
+++ app.config.ts
@@ -23,6 +23,7 @@ module.exports = ({ config }: ConfigContext): Partial<ExpoConfig> => {
     extra: {
       ...config.extra,
       BREEZ_API_KEY: process.env.BREEZ_API_KEY,
+      GEMINI_API_KEY: process.env.GEMINI_API_KEY,
       GROQ_API_KEY: process.env.GROQ_API_KEY,
     },
   }
