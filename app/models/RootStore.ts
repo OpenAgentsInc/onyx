4bba22a8758f735a6fcb0d9f9253d703396ae6fb
Christopher David <chris@openagents.com>
2024-12-24 10:12:11 -0600

    axe llamarn stuff and llmstore for now

--- app/models/RootStore.ts
+++ app/models/RootStore.ts
@@ -1,11 +1,9 @@
 import { Instance, SnapshotIn, SnapshotOut, types } from "mobx-state-tree"
 import { ChatStoreWithActions } from "./chat/ChatStore"
-import { LLMStoreModel } from "./llm/LLMStore"
 import { ToolStoreModel } from "./tools"
 
 export const RootStoreModel = types.model("RootStore").props({
   chatStore: types.optional(ChatStoreWithActions, {}),
-  llmStore: types.optional(LLMStoreModel, {}),
   toolStore: types.optional(ToolStoreModel, {})
 })
 
@@ -16,6 +14,5 @@ export interface RootStoreSnapshotIn extends SnapshotIn<typeof RootStoreModel> {
 export const createRootStoreDefaultModel = () =>
   RootStoreModel.create({
     chatStore: {},
-    llmStore: {},
     toolStore: {}
   })
