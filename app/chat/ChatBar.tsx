ee21f8b8d654d1273ec42724fc06121708e138d6
Christopher David <chris@openagents.com>
2024-12-27 19:47:36 -0600

    bar

--- app/chat/ChatBar.tsx
+++ app/chat/ChatBar.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native"
+import { Pressable, View } from "react-native"
 
 export const ChatBar = () => {
   return (
@@ -8,9 +8,21 @@ export const ChatBar = () => {
         bottom: 0,
         left: 0,
         right: 0,
-        height: 200,
-        backgroundColor: "#111",
+        height: 70,
       }}
-    ></View>
+    >
+      <Pressable>
+        <View
+          style={{
+            height: 40,
+            borderRadius: 20,
+            marginBottom: 30,
+            marginLeft: 20,
+            marginRight: 20,
+            backgroundColor: "#111",
+          }}
+        ></View>
+      </Pressable>
+    </View>
   )
 }
