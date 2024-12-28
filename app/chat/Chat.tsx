2a7b089e7d9daaa19098667f68678631a33e7f95
Christopher David <chris@openagents.com>
2024-12-28 09:57:50 -0600

    Add keyboard dismiss on touch outside in Chat.tsx to enhance UX.

--- app/chat/Chat.tsx
+++ app/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native"
+import { Keyboard, TouchableWithoutFeedback, View } from "react-native"
 import { useChat } from "@/hooks/useChat"
 import { ChatBar } from "./ChatBar"
 import { ChatOverlay } from "./ChatOverlay"
@@ -6,11 +6,13 @@ import { ChatOverlay } from "./ChatOverlay"
 export const Chat = () => {
   const { isLoading, messages } = useChat()
   return (
-    <View style={{ flex: 1, flexDirection: "column" }}>
-      <View style={{ flex: 1 }}>
-        <ChatOverlay messages={messages} isLoading={isLoading} />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={{ flex: 1, flexDirection: "column" }}>
+        <View style={{ flex: 1 }}>
+          <ChatOverlay messages={messages} isLoading={isLoading} />
+        </View>
+        <ChatBar />
       </View>
-      <ChatBar />
-    </View>
+    </TouchableWithoutFeedback>
   )
 }
