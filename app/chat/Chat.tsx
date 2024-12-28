35a780fd033237dbd42d47994457c784ff6be76d
Christopher David <chris@openagents.com>
2024-12-28 09:59:04 -0600

    Refactor Chat.tsx to nest TouchableWithoutFeedback inside a new View for improved layout control.

--- app/chat/Chat.tsx
+++ app/chat/Chat.tsx
@@ -6,13 +6,15 @@ import { ChatOverlay } from "./ChatOverlay"
 export const Chat = () => {
   const { isLoading, messages } = useChat()
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <View style={{ flex: 1, flexDirection: "column" }}>
-        <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, flexDirection: "column" }}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View style={{ flex: 1, zIndex: 1 }}>
           <ChatOverlay messages={messages} isLoading={isLoading} />
         </View>
+      </TouchableWithoutFeedback>
+      <View style={{ zIndex: 2 }}>
         <ChatBar />
       </View>
-    </TouchableWithoutFeedback>
+    </View>
   )
 }
