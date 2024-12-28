5e3eed27b457122abcd32019edf97c586f6a3af5
Christopher David <chris@openagents.com>
2024-12-27 20:46:09 -0600

    Refactor ChatBar to handle keyboard events based on OS type in ChatBar.tsx.

--- app/chat/ChatBar.tsx
+++ app/chat/ChatBar.tsx
@@ -27,7 +27,7 @@ export const ChatBar = () => {
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
-      keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 0}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 10 : 0}
       style={{
         position: "absolute",
         bottom: 0,
