3f9da6621a90513132beb475bab26ee95b926bff
Christopher David <chris@openagents.com>
2024-12-28 20:18:34 -0600

    Refactor Chat component to dismiss keyboard on press using Pressable from react-native.

--- app/chat/Chat.tsx
+++ app/chat/Chat.tsx
@@ -1,17 +1,22 @@
-import { View } from "react-native"
+import { Keyboard, Pressable, View } from "react-native"
 import { useChat } from "@/hooks/useChat"
 import { ChatBar } from "./ChatBar"
 import { ChatOverlay } from "./ChatOverlay"
-import { DismissKeyboardView } from "@/components/DismissKeyboardView"
 
 export const Chat = () => {
   const { isLoading, messages } = useChat()
   return (
-    <DismissKeyboardView style={{ flex: 1, flexDirection: "column" }}>
+    <Pressable 
+      style={{ flex: 1, flexDirection: "column" }}
+      onPress={() => {
+        console.log("Chat container pressed")
+        Keyboard.dismiss()
+      }}
+    >
       <View style={{ flex: 1 }}>
         <ChatOverlay messages={messages} isLoading={isLoading} />
       </View>
       <ChatBar />
-    </DismissKeyboardView>
+    </Pressable>
   )
 }
