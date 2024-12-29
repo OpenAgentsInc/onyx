d5d8c3b727989e36c2f733b7f3298a0725f6abf8
Christopher David <chris@openagents.com>
2024-12-28 20:12:19 -0600

    Wrapped Chat component in DismissKeyboardView for enhanced usability.

--- app/chat/Chat.tsx
+++ app/chat/Chat.tsx
@@ -2,15 +2,16 @@ import { View } from "react-native"
 import { useChat } from "@/hooks/useChat"
 import { ChatBar } from "./ChatBar"
 import { ChatOverlay } from "./ChatOverlay"
+import { DismissKeyboardView } from "@/components/DismissKeyboardView"
 
 export const Chat = () => {
   const { isLoading, messages } = useChat()
   return (
-    <View style={{ flex: 1, flexDirection: "column" }}>
+    <DismissKeyboardView style={{ flex: 1, flexDirection: "column" }}>
       <View style={{ flex: 1 }}>
         <ChatOverlay messages={messages} isLoading={isLoading} />
       </View>
       <ChatBar />
-    </View>
+    </DismissKeyboardView>
   )
 }
