f8e9ce1f480d1be4c1f3be82e5454792400bd292
GitHub API <noreply@github.com>
2024-12-30 14:11:21 -0600

    Delete app/models/wallet/types.ts

--- app/models/wallet/types.ts
+++
@@ -1,5 +0,0 @@
-import { Instance, SnapshotOut } from "mobx-state-tree"
-import { WalletStoreModel } from "./WalletStore"
-
-export interface WalletStoreType extends Instance<typeof WalletStoreModel> { }
-export interface WalletStoreSnapshot extends SnapshotOut<typeof WalletStoreModel> { }
