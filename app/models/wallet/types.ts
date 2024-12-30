ec0f9e6ee67cb73ceb4fed7763db4ccfcff72000
Christopher David <chris@openagents.com>
2024-12-30 14:12:07 -0600

    Create file app/models/wallet/types.ts

---
+++ app/models/wallet/types.ts
@@ -0,0 +1,32 @@
+import { IStateTreeNode, Instance } from "mobx-state-tree"
+import { WalletStoreModel } from "./WalletStore"
+
+// Wallet Store Types
+export interface IWalletStoreBase extends IStateTreeNode {
+  mnemonic: string | null
+  setMnemonic: (mnemonic: string) => void
+  setError: (message: string | null) => void
+  error: string | null
+}
+
+export interface IWalletStoreBalance extends IWalletStoreBase {
+  isInitialized: boolean
+  balanceSat: number
+  pendingSendSat: number
+  pendingReceiveSat: number
+}
+
+export interface IWalletStoreWithTransactions extends IWalletStoreBalance {
+  transactions: any[]
+}
+
+export interface IWalletStore extends IWalletStoreWithTransactions {
+  fetchBalanceInfo: () => Promise<void>
+  fetchTransactions: () => Promise<void>
+  sendPayment: (bolt11: string, amount: number) => Promise<void>
+  receivePayment: (amount: number, description?: string) => Promise<void>
+  setup: () => Promise<void>
+  disconnect: () => Promise<void>
+}
+
+export type WalletStore = Instance<typeof WalletStoreModel>
