a75da38ac458bc4bbdc742df992eed2efc3bd713
Christopher David <chris@openagents.com>
2024-12-30 13:54:19 -0600

    Refactor IWalletStore interface: add balance and pending transaction properties, include TransactionModel, and introduce setMnemonic method.

--- app/models/wallet/actions/types.ts
+++ app/models/wallet/actions/types.ts
@@ -1,8 +1,19 @@
 import { IStateTreeNode } from "mobx-state-tree"
+import { TransactionModel } from "../TransactionModel"
 
 export interface IWalletStore extends IStateTreeNode {
+  // Properties
   mnemonic: string | null
   isInitialized: boolean
+  balanceSat: number
+  pendingSendSat: number
+  pendingReceiveSat: number
+  transactions: {
+    clear: () => void
+  }
+
+  // Methods
+  setMnemonic: (mnemonic: string) => void
   setError: (message: string | null) => void
   fetchBalanceInfo: () => Promise<void>
 }
