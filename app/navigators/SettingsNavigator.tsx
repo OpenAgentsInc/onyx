1e3ed1100f9da0bc4c3dc010b80928861bd3b943
Christopher David <chris@openagents.com>
2024-12-30 20:09:09 -0600

    Added ShareScreen to SettingsNavigator and updated SettingsStackParamList.

--- app/navigators/SettingsNavigator.tsx
+++ app/navigators/SettingsNavigator.tsx
@@ -1,10 +1,12 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { SettingsScreen } from "@/screens/SettingsScreen/SettingsScreen"
 import { AutocoderSettings } from "@/screens/SettingsScreen/AutocoderSettings"
+import { ShareScreen } from "@/screens/ShareScreen/ShareScreen"
 
 export type SettingsStackParamList = {
   SettingsMenu: undefined
   AutocoderSettings: undefined
+  ShareScreen: undefined
 }
 
 const Stack = createNativeStackNavigator<SettingsStackParamList>()
@@ -19,6 +21,7 @@ export const SettingsNavigator = () => {
     >
       <Stack.Screen name="SettingsMenu" component={SettingsScreen} />
       <Stack.Screen name="AutocoderSettings" component={AutocoderSettings} />
+      <Stack.Screen name="ShareScreen" component={ShareScreen} />
     </Stack.Navigator>
   )
 }
