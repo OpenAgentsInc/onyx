f00f81ea2e65dd6ec350b6f1ad312fab691c44b8
Christopher David <chris@openagents.com>
2024-12-30 20:06:40 -0600

    Refactor styles.ts to include typography, update button and container styles, and adjust color scheme for improved UI consistency.

--- app/screens/SettingsScreen/styles.ts
+++ app/screens/SettingsScreen/styles.ts
@@ -1,24 +1,42 @@
 import { StyleSheet } from "react-native"
-import { colorsDark as colors } from "@/theme"
+import { colorsDark as colors, typography } from "@/theme"
 
 export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.palette.neutral50,
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
   button: {
-    backgroundColor: colors.primary,
+    backgroundColor: colors.backgroundSecondary,
     padding: 12,
-    borderRadius: 8,
+    borderRadius: 5,
     alignItems: "center",
+    marginBottom: 24,
   },
   buttonText: {
-    color: colors.textDark,
+    color: colors.palette.neutral800,
     fontSize: 16,
-    fontWeight: "600",
+    fontWeight: "bold",
+    fontFamily: typography.primary.normal,
   },
   menuItem: {
+    backgroundColor: colors.backgroundSecondary,
+    padding: 16,
+    borderRadius: 5,
     marginBottom: 12,
     borderWidth: 1,
-    borderColor: colors.border,
+    borderColor: colors.palette.neutral300,
+  },
+  menuItemText: {
+    color: colors.palette.neutral800,
+    fontSize: 16,
+    fontWeight: "bold",
+    fontFamily: typography.primary.normal,
   },
   text: {
-    color: colors.text,
+    fontFamily: typography.primary.normal,
+    color: colors.palette.neutral800,
   },
 })
