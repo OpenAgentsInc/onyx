13f4ebf362244a3baf63f291e5274960f394fec2
Christopher David <chris@openagents.com>
2024-12-30 18:15:32 -0600

    Create file app/chat/drawer/styles.ts

---
+++ app/chat/drawer/styles.ts
@@ -0,0 +1,58 @@
+import { StyleSheet } from "react-native"
+import { colorsDark as colors, typography } from "@/theme"
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    borderRightWidth: 1,
+    borderRightColor: colors.border,
+  },
+  topSection: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.border,
+  },
+  bottomSection: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderTopWidth: 1,
+    borderTopColor: colors.border,
+  },
+  newChatButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 16,
+  },
+  walletButton: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontFamily: typography.primary.medium,
+    color: "white",
+    marginLeft: 12,
+  },
+  scrollView: {
+    flex: 1,
+  },
+  chatItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.border,
+    backgroundColor: "transparent",
+  },
+  selectedChat: {
+    backgroundColor: colors.palette.neutral200,
+  },
+  chatPreviewText: {
+    color: "white",
+    fontFamily: typography.primary.medium,
+    marginBottom: 4,
+  },
+  dateText: {
+    color: colors.palette.neutral400,
+    fontSize: 12,
+  },
+})
