a379b369b5bf12e45e3341123d5b1d59a54a607b
Christopher David <chris@openagents.com>
2025-01-01 11:29:12 -0600

    Added NotificationsScreen to SettingsNavigator and updated SettingsStackParamList.

--- app/navigators/SettingsNavigator.tsx
+++ app/navigators/SettingsNavigator.tsx
@@ -2,11 +2,13 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { SettingsScreen } from "@/screens/SettingsScreen/SettingsScreen"
 import { AutocoderSettings } from "@/screens/SettingsScreen/AutocoderSettings"
 import { ShareScreen } from "@/screens/ShareScreen/ShareScreen"
+import { NotificationsScreen } from "@/screens/SettingsScreen/NotificationsScreen"
 
 export type SettingsStackParamList = {
   SettingsMenu: undefined
   AutocoderSettings: undefined
   ShareScreen: undefined
+  NotificationsScreen: undefined
 }
 
 const Stack = createNativeStackNavigator<SettingsStackParamList>()
@@ -22,6 +24,7 @@ export const SettingsNavigator = () => {
       <Stack.Screen name="SettingsMenu" component={SettingsScreen} />
       <Stack.Screen name="AutocoderSettings" component={AutocoderSettings} />
       <Stack.Screen name="ShareScreen" component={ShareScreen} />
+      <Stack.Screen name="NotificationsScreen" component={NotificationsScreen} />
     </Stack.Navigator>
   )
 }
