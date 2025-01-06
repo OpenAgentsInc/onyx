8a313de525445e9e363407f5379387c1e1aedf2d
Christopher David <chris@openagents.com>
2025-01-06 10:23:11 -0600

    input

--- app/osint/ChatDemo.tsx
+++ app/osint/ChatDemo.tsx
@@ -1,3 +1,57 @@
+import { ScrollView, View } from "react-native"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Text } from "@/components/ui/text"
+import { colors } from "@/theme/colorsDark"
+
+interface Message {
+  id: number
+  text: string
+  user: string
+}
+
+const messages = [
+  {
+    id: 1,
+    text: "Hello",
+    user: "Alice",
+  },
+  {
+    id: 2,
+    text: "Hi",
+    user: "Bob",
+  },
+]
+
+function Message({ message }: { message: Message }) {
+  return (
+    <View style={{ marginVertical: 15 }}>
+      <Text className="opacity-50">{message.user}</Text>
+      <Text>{message.text}</Text>
+    </View>
+  )
+}
+
 export function ChatDemo() {
-  return <></>
+  return (
+    <ScrollView
+      style={{
+        margin: 20,
+        width: 400,
+        height: 300,
+        paddingVertical: 10,
+      }}
+    >
+      <Card>
+        <CardHeader>
+          <CardTitle>UAP Sensemaking</CardTitle>
+          <CardDescription>Nostr NIP-28 Chat Channel</CardDescription>
+        </CardHeader>
+        <CardContent>
+          {messages.map((message) => (
+            <Message key={message.id} message={message} />
+          ))}
+        </CardContent>
+      </Card>
+    </ScrollView>
+  )
 }
