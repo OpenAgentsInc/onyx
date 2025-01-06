cb08f2c7520c590cf5f1e69667939fa11c77f5c1
Christopher David <chris@openagents.com>
2025-01-06 10:31:29 -0600

    Create file app/osint/styles.ts

---
+++ app/osint/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: "100%",
+    maxWidth: 800,
+    alignSelf: "center",
+    width: "100%",
+  },
+  messagesContainer: {
+    flex: 1,
+    padding: 20,
+  },
+  inputContainer: {
+    padding: 20,
+    borderTopWidth: 1,
+    borderTopColor: "#2a2a2a",
+  },
+  scrollView: {
+    flex: 1,
+  },
+})
