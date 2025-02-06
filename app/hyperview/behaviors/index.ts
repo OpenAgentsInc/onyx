2e8354f715c36ede421fba402d0e5884d2c041ff
Christopher David <chris@openagents.com>
2025-02-06 06:33:55 -0600

    Update index.ts

--- app/hyperview/behaviors/index.ts
+++ app/hyperview/behaviors/index.ts
@@ -1,10 +1,13 @@
 import { Behavior } from 'hyperview/src/types'
 
-const behaviors: Array<[string, Behavior]> = [
-  ['hv-alert', (element, context) => {
+const behaviors: Record<string, Behavior> = {
+  'hv-alert': (element, context) => {
     const message = element.getAttribute('message') || 'Alert'
     alert(message)
-  }],
-]
+  },
+}
 
-export default behaviors
+export const getRegistry = (customBehaviors: Record<string, Behavior> = {}) => ({
+  ...behaviors,
+  ...customBehaviors,
+})
