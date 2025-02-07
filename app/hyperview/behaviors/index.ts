048810278ab034df852b4487a972a2e6afa3e666
Christopher David <chris@openagents.com>
2025-02-07 12:40:17 -0500

    Update index.ts

--- app/hyperview/behaviors/index.ts
+++ app/hyperview/behaviors/index.ts
@@ -1,9 +1,11 @@
 import { AddStyles } from './AddStyles';
 import WebSocketBehaviors from './WebSocket';
 import { OpenUrlBehavior } from './OpenUrl';
+import { AuthBehavior } from './Auth';
 
 export default [
   AddStyles,
   ...WebSocketBehaviors,
   OpenUrlBehavior,
+  AuthBehavior,
 ];
