871fd264864f223de6959c81a22f8acd163838bc
Christopher David <chris@openagents.com>
2025-02-07 10:42:12 -0500

    Update index.ts

--- app/hyperview/behaviors/index.ts
+++ app/hyperview/behaviors/index.ts
@@ -1,13 +1,13 @@
 import { AddStyles } from './AddStyles';
 import WebSocketBehaviors from './WebSocket';
-import { openUrl } from './OpenUrl';
-import { navigate } from './Navigate';
-import { fetch } from './Fetch';
+import { OpenUrlBehavior } from './OpenUrl';
+import { NavigateBehavior } from './Navigate';
+import { FetchBehavior } from './Fetch';
 
-// Export behaviors in the format Hyperview expects
-export default {
-  'open-url': openUrl.callback,
-  'navigate': navigate.callback,
-  'fetch': fetch.callback,
+export default [
+  AddStyles,
   ...WebSocketBehaviors,
-};
+  OpenUrlBehavior,
+  NavigateBehavior,
+  FetchBehavior,
+];
