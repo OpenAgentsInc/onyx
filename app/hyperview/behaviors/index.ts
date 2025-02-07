14d322409ea1655ee33e35c65eda6a9fc6381d23
Christopher David <chris@openagents.com>
2025-02-07 10:46:03 -0500

    Update index.ts

--- app/hyperview/behaviors/index.ts
+++ app/hyperview/behaviors/index.ts
@@ -4,10 +4,19 @@ import { OpenUrlBehavior } from './OpenUrl';
 import { NavigateBehavior } from './Navigate';
 import { FetchBehavior } from './Fetch';
 
-export default [
+console.log('[Behaviors] Registering behaviors...');
+console.log('[Behaviors] OpenUrlBehavior:', OpenUrlBehavior);
+console.log('[Behaviors] NavigateBehavior:', NavigateBehavior);
+console.log('[Behaviors] FetchBehavior:', FetchBehavior);
+
+const behaviors = [
   AddStyles,
   ...WebSocketBehaviors,
   OpenUrlBehavior,
   NavigateBehavior,
   FetchBehavior,
 ];
+
+console.log('[Behaviors] Final behaviors array:', behaviors);
+
+export default behaviors;
